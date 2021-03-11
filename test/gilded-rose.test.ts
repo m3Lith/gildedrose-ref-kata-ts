e324efb552c21b6d6a15a439772480e4580f51d1
m3Lith <ekstras@gmail.com>
2021-03-11 12:17:29 +0200

    Use jest for tests

--- test/gilded-rose.test.ts
+++ test/gilded-rose.test.ts
@@ -1,4 +1,3 @@
-import { expect } from 'chai';
 import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
@@ -6,7 +5,7 @@ describe('Gilded Rose', function () {
     it('should foo', function() {
         const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+        expect(items[0].name).toEqual('fixme');
     });
 
 });
