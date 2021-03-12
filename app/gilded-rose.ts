58d5c17c0c9c5e792f26bf13f9fb8f82a46626ee
m3Lith <ekstras@gmail.com>
2021-03-12 11:45:43 +0200

    Move quality calculation to separate functions

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -18,27 +18,11 @@ export default class GildedRose {
   public updateQuality(): Item[] {
     for (const item of this.items) {
       if (item.name !== SpecialItemTypes.AgedBrie && item.name !== SpecialItemTypes.BackStagePasses) {
-        if (item.quality > 0) {
-          if (item.name !== SpecialItemTypes.Sulfuras) {
-            item.quality = item.quality - 1
-          }
-          if (item.name === SpecialItemTypes.Conjured && item.quality > 0) {
-            item.quality = item.quality - 1
-          }
-        }
+        item.quality = GildedRose.decreaseQuality(item)
       } else if (item.quality < MAX_QUALITY) {
         item.quality = item.quality + 1
         if (item.name === SpecialItemTypes.BackStagePasses) {
-          if (item.sellIn <= BCKST_PASS_TRIGGER_10) {
-            if (item.quality < MAX_QUALITY) {
-              item.quality = item.quality + 1
-            }
-          }
-          if (item.sellIn <= BCKST_PASS_TRIGGER_5) {
-            if (item.quality < MAX_QUALITY) {
-              item.quality = item.quality + 1
-            }
-          }
+          item.quality = GildedRose.adjustBackStagePasses(item)
         }
       }
       if (item.name !== SpecialItemTypes.Sulfuras) {
@@ -47,14 +31,7 @@ export default class GildedRose {
       if (item.sellIn < 0) {
         if (item.name !== SpecialItemTypes.AgedBrie) {
           if (item.name !== SpecialItemTypes.BackStagePasses) {
-            if (item.quality > 0) {
-              if (item.name !== SpecialItemTypes.Sulfuras) {
-                item.quality = item.quality - 1
-              }
-              if (item.name === SpecialItemTypes.Conjured && item.quality > 0) {
-                item.quality = item.quality - 1
-              }
-            }
+            item.quality = GildedRose.decreaseQuality(item)
           } else {
             item.quality = item.quality - item.quality
           }
@@ -65,5 +42,31 @@ export default class GildedRose {
     }
 
     return this.items
+  }
+
+  private static decreaseQuality(item: Item): number {
+    let { quality } = item
+
+    if (quality > 0 && item.name !== SpecialItemTypes.Sulfuras) {
+      quality = quality - 1
+      if (item.name === SpecialItemTypes.Conjured && quality > 0) {
+        quality = quality - 1
+      }
+    }
+
+    return quality
+  }
+
+  private static adjustBackStagePasses(item: Item): number {
+    let { quality } = item
+
+    if (item.sellIn <= BCKST_PASS_TRIGGER_10 && quality < MAX_QUALITY) {
+      quality = quality + 1
+    }
+    if (item.sellIn <= BCKST_PASS_TRIGGER_5 && quality < MAX_QUALITY) {
+      quality = quality + 1
+    }
+
+    return quality
   }
 }
